a9905b6be09b935ffaf58b0677c0ee0552bf3127
overclock07 <sunjen08@gmail.com>
2018-04-23 11:04:46 +0000

    Sql database

--- app/views.py
+++ app/views.py
@@ -34,22 +34,34 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit(): #if form.username.data:
             # Get the username and password values from the form.
-
+            usrname = form.username.data
+            psword = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username=usrname, password=psword).first()
+            
+            if user is not None:
             # get user id, load into session
-            login_user(user)
-
+             login_user(user)
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            
+             flash('Logged in successfully.', 'success')
+             return redirect(url_for("secure_page")) # they should be redirected to a secure-page route instead
+            else:
+             flash('Username/Password is incorrect.', 'danger')
     return render_template("login.html", form=form)
 
-
+@app.route('/secure-page/')
+@login_required
+def secure_page():
+    if current_user.is_authenticated:
+        return render_template('secure_page.html')
+    else:
+        return render_template('login.html')
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
